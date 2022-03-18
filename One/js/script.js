9a591f09266cf91f82e5a03c715e054f72c3df14
Uygar Pinar <uygrpinar@gmail.com>
2022-03-18 19:24:31 +0300

    first commit

---
+++ One/js/script.js
@@ -0,0 +1,29 @@
+var img = document.getElementById('logo-image')
+
+var menuLink = document.getElementById('menuLink')
+
+var headerContainer = document.getElementById('headerDivId');
+
+var logoContainer = document.getElementById('logoDivId')
+
+var imageContainer =document.getElementById('imageDivId')
+
+var menuContainer = document.getElementById('menuDivId')
+
+img.addEventListener("click",changeScreen);
+
+function changeScreen(e){
+    
+    headerContainer.style.height = '100%'
+    logoContainer.style.justifyContent='center'
+    imageContainer.style.height = '30rem';
+    imageContainer.style.width = '30rem';
+    
+    // img.style.width= '250px'
+    img.style.opacity = '1'
+    img.style.cursor = 'default'
+
+    menuLink.style.opacity = '1'
+    menuContainer.style.display ='grid'
+}
+
